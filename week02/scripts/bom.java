0fcb3adff527b5624169ebf3ea99d03fbee7e306
adeoyej1 <johnadeoye55@gmail.com>
2026-09-14 17:31:59 +0100

    week02

---
+++ week02/scripts/bom.java
@@ -0,0 +1,26 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+const li = document.createElement('li');
+
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value;
+deleteButton.textContent = '❌';
+
+li.append(deleteButton);
+
+list.append(li);
+
+const deleteButton = document.createElement('button');
+
+deleteButton.textContent = '❌';
+
+deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
+
+deleteButton.addEventListener('click', function () {
+  list.removeChild(li);
+  input.focus();
+});

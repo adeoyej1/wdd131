a6ec8ab71c85eff265387cc275d60bea7188c3e3
adeoyej1 <johnadeoye55@gmail.com>
2026-09-24 17:32:51 +0100

    place.html

--- Week03/script/place.js
+++ Week03/script/place.js
@@ -1,28 +1,62 @@
-// Current year
+
+// ------------------------------
+// Current Year
+// ------------------------------
+
 const currentYear = new Date().getFullYear();
+
 document.querySelector("#currentyear").textContent = currentYear;
 
-// Last modified date
+
+// ------------------------------
+// Last Modified Date
+// ------------------------------
+
 document.querySelector("#lastModified").textContent =
     `Last Modification: ${document.lastModified}`;
 
-// Wind chill calculation
-const temperature = 31;
-const windSpeed = 8;
 
+// ------------------------------
+// Wind Chill Calculation
+// ------------------------------
+
+const temperature = Number(
+    document.querySelector("#temp").textContent
+);
+
+const windSpeed = Number(
+    document.querySelector("#wind").textContent
+);
+
+const windChillElement = document.querySelector("#windchill");
+
+
+// Wind chill formula
 function calculateWindChill(temp, wind) {
     return (
         13.12 +
-        0.6215 * temp -
-        11.37 * Math.pow(wind, 0.16) +
-        0.3965 * temp * Math.pow(wind, 0.16)
+        (0.6215 * temp) -
+        (11.37 * Math.pow(wind, 0.16)) +
+        (0.3965 * temp * Math.pow(wind, 0.16))
     ).toFixed(1);
 }
 
+
+// Wind chill is only calculated when:
+// Temperature <= 10°C
+// AND wind speed > 4.8 km/h
+
 if (temperature <= 10 && windSpeed > 4.8) {
-    document.querySelector("#windchill").textContent =
-        `${calculateWindChill(temperature, windSpeed)} °C`;
+
+    const windChill = calculateWindChill(
+        temperature,
+        windSpeed
+    );
+
+    windChillElement.textContent = `${windChill} °C`;
+
 } else {
-    document.querySelector("#windchill").textContent =
-        "N/A";
+
+    windChillElement.textContent = "N/A";
 }
+```
